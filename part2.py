c6101554043c4eb72095ae28533788613fe1d701
Suraj Kattige <suraj.kattige056@gmail.com>
2018-05-01 10:13:44 -0700

    Adding comments for better understanding

--- part2.py
+++ part2.py
@@ -3,8 +3,6 @@ import sys
 
 
 def type_of_partition(a):
-    output = ""
-    code = "0"
     if(a == 0x01):
         output = "DOS 12-bit FAT"
         code = "01"
@@ -129,15 +127,16 @@ def partition_table(byte_name):
         i += 1  #460
         nbr_sec_part3 = (byte_name[i])#1f
         i += 1  #461
-        # print("value of i at the end: %s" %i)
         nbr_sec_part4 = (byte_name[i]) #00
         i += 1
         nbr_sec_part = hex((((nbr_sec_part4 << 24) | nbr_sec_part3 << 16) | nbr_sec_part2 << 8) | nbr_sec_part1)
 
-        start_sect_addr = int(nbr_sec_mbr, 16) #TODO: Add the calculation part
-        size_of_partition = int(nbr_sec_part, 16) #TODO: Add the calculation part
+        start_sect_addr = int(nbr_sec_mbr, 16)
+        size_of_partition = int(nbr_sec_part, 16)
+
         if((tof != 0) and (tof != "Error")):
             print("(%s) %s, %s, %s" %(code, tof, start_sect_addr, size_of_partition))
+
             if((code == "04") or (code == "06") or (code == "0B") or (code == "0C") or (code == "1B") or (code == "86")):
                 fat_partition(code, byte_name, start_sect_addr, size_of_partition)
 
@@ -145,39 +144,43 @@ def partition_table(byte_name):
     return 0
 
 def fat_partition(code, byte_name, start_sect_addr, size_of_partition):
-    start_sect = 0
+    start_sect = 0 #Reserved area always starts at sector 0
     size = ((byte_name[start_sect_addr + 15] << 8 )| byte_name[start_sect_addr + 14])
+
     if (start_sect + size) < 1:
         end_sect = 0
     else:
         end_sect = start_sect + size - 1
+
     sect_per_cluster = byte_name[start_sect_addr + 13]
-    fat_area = 0
     no_of_fat = byte_name[start_sect_addr + 16]
+
     if ((code == "04") or (code == "06")  or (code == "86")):
         size_of_fat = ((byte_name[start_sect_addr + 23] << 8) | byte_name[start_sect_addr + 22])
     else:
         size_of_fat = ((((byte_name[start_sect_addr + 35] << 24) | (byte_name[start_sect_addr + 34] << 16)) | (byte_name[start_sect_addr + 33] << 8)) | (byte_name[start_sect_addr + 32]))
-    if no_of_fat == 0:
+
+    if no_of_fat == 0: #If number of FATs is 0, Then FAT start and end sectors will be 0
         fat_start_sect = 0
         fat_end_sect = 0
     else:
-        fat_start_sect = end_sect + 1
-        fat_end_sect = (size_of_fat * no_of_fat) + fat_start_sect - 1
-    if fat_end_sect == 0:
+        fat_start_sect = end_sect + 1 #FAT sector address starts after the reserved area. hence endsect + 1
+        fat_end_sect = (size_of_fat * no_of_fat) + fat_start_sect - 1   #FAT end sector would be (number of fat * size of one FAT) + fat start sector address - 1
+
+    if fat_end_sect == 0: #If there are no FAT files, then first sector of cluster 2 will be 0 as well
         first_sect_cluster = 0
     else:
-        if ((code == "04") or (code == "06") or (code == "86")):
-            no_of_files_direct = ((byte_name[start_sect_addr + 18] << 8) | byte_name[start_sect_addr + 17])
-            bytes_per_sect = ((byte_name[start_sect_addr + 12] << 8) | byte_name[start_sect_addr + 11])
-            no_of_files_sect = no_of_files_direct / bytes_per_sect
-            first_sect_cluster = fat_end_sect + no_of_files_sect - 1
-        else:
-            first_sect_cluster = fat_end_sect + 1
+        if ((code == "04") or (code == "06") or (code == "86")): #If it is a FAT 16 file type
+            no_of_files_direct = ((byte_name[start_sect_addr + 18] << 8) | byte_name[start_sect_addr + 17]) #Number of files in the root directory
+            bytes_per_sect = ((byte_name[start_sect_addr + 12] << 8) | byte_name[start_sect_addr + 11]) #bytes per sector in the file system
+            no_of_files_sect = no_of_files_direct / bytes_per_sect  #number of files in root directory in sectors
+            first_sect_cluster = fat_end_sect + no_of_files_sect - 1    #First sector of cluster 2 will be fat end sector address + number of root directory files in sectors - 1
+        else:   #If the file system is of type FAT 32
+            first_sect_cluster = fat_end_sect + 1   #First sector of the cluster 2 will be end sector of FAT + 1
 
     print("Reserved area:\tStart sector: %s\tEnding sector: %s\tSize: %s" %(start_sect, end_sect, size))
     print("Sectors per cluster: %s sectors" %(sect_per_cluster))
-    print("FAT area: %s\tStart sector: %s\tEnding sector: %s" %(fat_area, fat_start_sect, fat_end_sect))
+    print("FAT area: \tStart sector: %s\tEnding sector: %s" %(fat_start_sect, fat_end_sect))
     print("# of FATs: %s" %(no_of_fat))
     print("The size of each FAT: %s sectors" %(size_of_fat))
     print("The first sector of cluster 2: %s sectors" %(first_sect_cluster))
@@ -194,7 +197,6 @@ def main():
     md5 = hashlib.md5()
     sha1 = hashlib.sha1()
 
-    # with open(sys.argv[1], 'rb') as f:
     with open(sys.argv[1], 'rb') as f:
         while True:
             data = f.read(BUF_SIZE)
@@ -210,7 +212,6 @@ def main():
     # --------------------Requirement 3---------------------#
     print("# --------------------Requirement 3---------------------#")
     byte_name = filename.read()
-    # print("Type of partition: %s " %hex(byte_name[450]))
     partition_table(byte_name)
 
     return 0
